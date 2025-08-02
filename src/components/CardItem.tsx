eea2f1da3030b1ad76bb0b7cbfb12540930c219d
tony nguyen <nhattruong1811@gmail.com>
2025-08-02 11:39:07 +0700

    ui trello

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,48 +1,29 @@
-import { Draggable } from 'react-beautiful-dnd';
 import { DeleteOutlined, EditOutlined, SettingOutlined } from '@ant-design/icons'
 import { Avatar, Card } from 'antd'
 import Meta from 'antd/es/card/Meta'
-import type { ICardItem } from '../types/index.td'
 
-interface CardItemProps {
-  index: number,
-  card: ICardItem
-}
-
-
-function CardItem({ index, card }: CardItemProps) {
+function CardItem() {
   return (
-    <Draggable draggableId={String(card.id)} index={index}>
-      {(provided) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          className='card_item'
-        >
-          <Card
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <SettingOutlined key="setting" />,
-              <EditOutlined key="edit" />,
-              <DeleteOutlined key="delete" />,
-            ]}
-          >
-            <Meta
-              avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
-              title={card.title}
-              description="This is the description"
-            />
-          </Card>
-        </div>
-      )}
-    </Draggable>
-    
+    <Card
+      cover={
+        <img
+          alt="example"
+          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+        />
+      }
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <DeleteOutlined key="delete" />,
+      ]}
+      className='card_item'
+    >
+      <Meta
+        avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+        title="Card title"
+        description="This is the description"
+      />
+    </Card>
   )
 }
 
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,68 +1,29 @@
-import { Draggable, Droppable } from 'react-beautiful-dnd'
 import { DeleteOutlined, PlusOutlined } from '@ant-design/icons'
 import { Button, Card, Tooltip } from 'antd'
-
-import type { ICardItem, ListItem } from '../types/index.td'
 import CardItem from './CardItem'
 
-interface CardListProps {
-  index: number,
-  listItem: ListItem,
-  cards: ICardItem[]
-}
-
-function CardList({ index, listItem, cards }: CardListProps) {
-
-  console.log('cards: ', cards)
+function CardList() {
   return (
-   <Draggable draggableId={String(listItem.id)} index={index}>
-    {(provided) => (
-      <div
-        ref={provided.innerRef}
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-      >
-        <Droppable droppableId={String(listItem.id)} type="CARD" direction='vertical'>
-          {(provided) => (
-            <Card 
-              size="small" 
-              title={listItem.title}
-              extra={
-                <>
-                  <Tooltip title="Add Card">
-                    <Button shape="circle" size='small' icon={<PlusOutlined />} style={{ marginRight: 4 }} />
-                  </Tooltip>
-                  <Tooltip title="Delete List">
-                    <Button danger shape="circle" size='small' icon={<DeleteOutlined />} />
-                  </Tooltip>
-                </>
-              } 
-              className='card_list'
-            >
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                className='card_list_container'
-              >
-                {cards.map((card, cardIndex) => {
-                  return (
-                    <CardItem 
-                      key={card.id}
-                      index={cardIndex}
-                      card={card}
-                    />
-                  )
-                })}
-                {provided.placeholder}
-              </div>
-            </Card>
-           
-          )}
-        </Droppable>
+    <Card 
+      size="small" 
+      title="Todo" 
+      extra={
+        <>
+          <Tooltip title="Add Card">
+            <Button shape="circle" size='small' icon={<PlusOutlined />} style={{ marginRight: 4 }} />
+          </Tooltip>
+          <Tooltip title="Delete List">
+            <Button danger shape="circle" size='small' icon={<DeleteOutlined />} />
+          </Tooltip>
+        </>
+      } 
+      className='card_list'
+    >
+      <div className='card_list_container'>
+        <CardItem />
+        <CardItem />
       </div>
-    )}
-  </Draggable>
-    
+    </Card>
   )
 }
 
